ebede64396723b4de1a6cba6e92f49bc50b84c3a
fossi13 <fossi13@users.noreply.github.com>
2024-07-03 16:58:22 +0700

    fix:finish for loop

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+function printPattern(n) {
+    let result = "" ;
+   for (let i = 1 ; i <= n ; i++) {
+    for(let j = 1; j <= i ; j++) {
+        result += j + ' ';
+        
+    }
+        result = result.trim() + '\n';
 }
+        return result.trim()
+}
+
+console.log(printPattern(5))
--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
+function printStar(n) {
+    let result = "" ;
+   for (let i = 1 ; i <= n ; i++) {
+    for(let j = 1; j <= i ; j++) {
+        result +=  '* ';
+        
+    }
+        result = result.trim() + '\n';
+}
+        return result.trim()
    
 }
+
+console.log(printStar(5))
